2192d2adc5c9aa39d4af339a42041f5721953510
Larissa-Djunbawski <larydjm@gmail.com>
2024-08-20 22:00:38 -0300

    codigo das 3 entidades

--- src/controllers/funcionarios.js
+++ src/controllers/funcionarios.js
@@ -1,31 +1,23 @@
 const db = [];
-
 let nextId = 1;
 
 const model = (body, id = nextId++) => {
-  if (
-    body.cor != undefined &&
-    body.nome != undefined &&
-    body.cor != "" &&
-    body.nome != ""
-  ) {
+  if (body.nome != undefined && body.cargo != undefined) {
     return {
       id,
       nome: body.nome,
-      cor: body.cor,
+      cargo: body.cargo,
+      setorId: body.setorId // Referência ao Setor
     };
   }
 };
 
 const store = (body) => {
   const novo = model(body);
-
   if (novo) {
     db.push(novo);
-
     return 201;
   }
-
   return 400;
 };
 
@@ -36,23 +28,19 @@ const show = (id) => db.find((el) => el.id == id);
 const update = (id, body) => {
   const index = db.findIndex((el) => el.id == id);
   const novo = model(body, parseInt(id));
-
   if (index != -1 && novo) {
     db[index] = novo;
     return 200;
   }
-
   return 400;
 };
 
 const destroy = (id) => {
   const index = db.findIndex((el) => el.id == id);
-
-  if(index != -1) {
+  if (index != -1) {
     db.splice(index, 1);
     return 200;
   }
-
   return 404;
 };
 
@@ -62,4 +50,4 @@ module.exports = {
   show,
   update,
   destroy
-}
+};
